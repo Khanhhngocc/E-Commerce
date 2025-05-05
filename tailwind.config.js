c5698cb715db2391d63a3400807fc1c3f9948866
Khanhhngocc <ngoctaehyung0630@gmail.com>
2025-05-05 19:24:42 +0700

    Header

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+export default {
+    content: [
+
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#ff5252'
+            },
+            backgroundColor: {
+                primary: '#ff5252'
+            }
+        }
+    }
+}
